c9c3ef493fa015e334827de42dcb006b07ac28e4
Tedr <avpokrov@gmail.com>
2024-08-13 07:51:25 +0300

    feat: добавить в класс карточек методы рендера и обработчиков событий

--- scripts/Card.js
+++ scripts/Card.js
@@ -3,7 +3,7 @@ export class Card {
         this._name = dataCard.name;
         this._link = dataCard.link;
         this._template = template;
-    } 
+    }
     _getTemplate() {
         const cardElement = document.querySelector(this._template).content
                      .querySelector('.card').cloneNode(true);
@@ -12,6 +12,7 @@ export class Card {
 
     generateCard(){
         this._element = this._getTemplate();
+        this._setEventListeners();
         const cardImg =  this._element.querySelector('.card__img');
         const cardName = this._element.querySelector('.card__name');
         cardImg.src = this._link;
@@ -20,4 +21,33 @@ export class Card {
         return this._element;
     }
 
-}
+    _setEventListeners(){
+      this._element.querySelector('.like').addEventListener('click', () => {
+        this._handleClickLike();
+      })
+      this._element.querySelector('.trash').addEventListener('click', ()=> {
+        this._element.remove();
+      })
+      this._element.querySelector('.card__img').addEventListener('click', ()=> {
+        this._openPopupImage();
+      })
+    }
+
+    _handleClickLike(){
+      this._element.querySelector('.like').classList.toggle('like_active');
+    }
+
+    _handleClickTrash(){
+      this._element.remove();
+    }
+
+    _openPopupImage(){
+      const popup = document.querySelector('.popup-img');
+      const image = popup.querySelector('.popup-img__image');
+      image.src = this._link;
+      image.alt = this._name;
+      popup.querySelector('.popup-img__text').textContent = this._name;
+      popup.classList.add('popup_opened');
+    }
+
+ }
